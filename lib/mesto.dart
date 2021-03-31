f8c0a2632cf201747845a4d79af496aaa47ea97f
Dan Marek <danmarek@gmail.com>
2021-03-31 14:16:21 +0200

    add shared_preferences

--- lib/mesto.dart
+++ lib/mesto.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MestoPage extends StatefulWidget {
   MestoPage({Key key, this.title}) : super(key: key);
